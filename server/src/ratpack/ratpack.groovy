3d753d7eef58351c6b66fc130c9555fb13770a7a
tomaslin <tomaslin@gmail.com>
2013-07-31 22:36:26 +0000

    holy shit, got gridfs working

--- server/src/ratpack/ratpack.groovy
+++ server/src/ratpack/ratpack.groovy
@@ -7,6 +7,15 @@ import com.mongodb.BasicDBObject
 import com.mongodb.DBCollection
 import com.mongodb.util.JSON
 import org.bson.types.ObjectId
+import com.mongodb.gridfs.GridFSInputFile
+import com.mongodb.gridfs.GridFSDBFile
+import com.mongodb.gridfs.GridFS
+import sun.misc.BASE64Decoder
+import io.netty.buffer.ByteBuf
+import io.netty.buffer.ByteBufOutputStream
+import java.util.UUID
+import io.netty.buffer.Unpooled
+import org.apache.commons.io.IOUtils
 
 def indexPages = ["index.html"] as String[]
 
@@ -14,6 +23,8 @@ MongoClient mongo = new MongoClient('ds037468.mongolab.com', 37468)
 DB db = mongo.getDB('ratpack')
 db.authenticate('ratpack', 'ratpack'.toCharArray())
 DBCollection personCollection = db.getCollection("person")
+GridFS imageStore = new GridFS(db, 'images')
+BASE64Decoder decoder = new BASE64Decoder()
 
 ratpack {
     handlers {
@@ -23,7 +34,7 @@ ratpack {
         }
 
         // list of people
-        get("api/person/") {
+        get("api/person") {
             def cursor = personCollection.find(new BasicDBObject(), new BasicDBObject("name", 1).append("image", 1))
             def items = []
             try {
@@ -54,6 +65,28 @@ ratpack {
             }
         }
 
-        assets "public", indexPages
+        // save a base64 encoded image via gridfs
+        post("api/image") {
+          def image = decoder.decodeBuffer(request.text as String)
+          String filename = UUID.randomUUID()
+          GridFSInputFile file = imageStore.createFile(image)
+          file.setFilename(filename)
+          file.setContentType("image/png")
+          file.save()
+          response.send 'application/json', "{'filename':'${filename}'}"
+        }
+
+        // render the image in ratpack
+        get("api/image/:filename") {
+          GridFSDBFile image = imageStore.findOne(pathTokens.filename)
+          if( image ){
+            byte[] imageBytes = IOUtils.toByteArray(image.inputStream)
+            response.send 'image/png', Unpooled.wrappedBuffer(imageBytes)            
+          } else {
+            clientError 404
+          }
+       }
+
+       assets "public", indexPages
     }
 }
