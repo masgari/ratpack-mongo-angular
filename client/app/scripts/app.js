eb6281ece85c46994c8d05bf70bd27f1984db697
tomaslin <tomaslin@gmail.com>
2013-07-31 13:18:11 +0000

    cleanup of scaffolded data in angular app

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -4,10 +4,6 @@ angular.module('initApp', [])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/person', {
         templateUrl: 'views/person.html',
         controller: 'PersonCtrl'
       })
--- client/app/scripts/controllers/person.js
+++ client/app/scripts/controllers/person.js
@@ -10,4 +10,16 @@ angular.module('initApp')
       {name:'Hulk', availabilityCount: 5},
       {name:'Ant Man', availabilityCount: 5}
     ];
+    
+    $scope.selectedPerson = {
+      picture : "http://static.comicvine.com/uploads/original/11/117763/2786642-tumblr_mg2iesn3go1qiknbco1_1280.jpg",
+      name : "Thor",
+      department : "Avengers",
+      job : "God of Thunder",
+      availability : [
+         'jan 2, 2014',
+         'may 4, 2014'
+      ]
+    }
+    
   });
