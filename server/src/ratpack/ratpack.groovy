e4257b58dd4548614065d8632d48697ee4514da0
tomaslin <tomaslin@gmail.com>
2013-08-03 21:32:07 +0000

    Make mongoservice a global service

--- server/src/ratpack/ratpack.groovy
+++ server/src/ratpack/ratpack.groovy
@@ -11,30 +11,30 @@ ratpack {
         register new MongoModule(new File('config.groovy'))
     }
 
-    handlers {
+    handlers { MongoService mongoService ->
 
         prefix('api') {
 
             // validation method to make sure we can connect to our DB
-            get('collections') { MongoService mongoService ->
+            get('collections') {
                 def collections = mongoService.collections()
                 response.send "Available database collections are $collections"
             }
 
             prefix('person') {
 
-                get { MongoService mongoService ->
+                get {
                     response.send 'application/json', mongoService.find('person', '{}', '{"name":1,"image":1}')
                 }
 
-                post { MongoService mongoService ->
+                post {
                     accepts.type('application/json') {
                         mongoService.save('person', request.text)
                         response.send()
                     }.send()
                 }
 
-                get(':id') { MongoService mongoService ->
+                get(':id') {
                     def queryById = """{ "_id": { "\$oid": "${pathTokens.id}" }}"""
                     def person = mongoService.findOne('person', queryById)
                     if (person) {
@@ -48,13 +48,13 @@ ratpack {
 
             prefix('image') {
 
-                post { MongoService mongoService ->
+                post {
                     def image = decoder.decodeBuffer(request.text as String)
                     def filename = mongoService.createFile('images', image, contentType)
                     response.send 'application/json', "{'filename':'${filename}'}"
                 }
 
-                get(':filename') { MongoService mongoService ->
+                get(':filename') {
                     def image = mongoService.fetchFile('images', pathTokens.filename)
                     if(image){
                         byte[] imageBytes = org.apache.commons.io.IOUtils.toByteArray(image.inputStream)
